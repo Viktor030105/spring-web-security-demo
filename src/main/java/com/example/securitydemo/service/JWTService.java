a42b5e7e986bee243a6b94326cffb4c8c224702f
Viktor Angelov <vikotor05@gmail.com>
2025-05-08 22:15:57 +0300

    Generated JWT token

--- src/main/java/com/example/securitydemo/service/JWTService.java
+++ src/main/java/com/example/securitydemo/service/JWTService.java
@@ -1,10 +1,49 @@
 package com.example.securitydemo.service;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.io.Encoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.KeyGenerator;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class JWTService {
-    public String generateToken() {
-        return "token";
+
+    private String secretKey = "";
+
+    public JWTService() {
+        try {
+            KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
+            secretKey = Encoders.BASE64.encode(keyGen.generateKey().getEncoded());
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String generateToken(String username) {
+
+        Map<String, Object> claims = new HashMap<>();
+
+        return Jwts.builder()
+                .claims()
+                .add(claims)
+                .subject(username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 60 * 60 * 30))
+                .and()
+                .signWith(getKey())
+                .compact();
+    }
+
+    private Key getKey() {
+        byte[] secretKeyByte = Decoders.BASE64.decode(this.secretKey);
+        return Keys.hmacShaKeyFor(secretKeyByte);
     }
 }
